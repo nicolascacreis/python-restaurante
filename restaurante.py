411fb4d9cda4098a5b9dc92e5811a482ebd70f1d
nicolascacreis <reisnicolas112@gmail.com>
2025-02-17 20:25:49 -0300

    Update restaurante.py

---
+++ restaurante.py
@@ -0,0 +1,14 @@
+class Restaurante: 
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_panama = Restaurante()
+restaurante_panam.nome = 'nome'
+restaurante_panama.categoria = 'Culturas'
+
+restaurante_brasil = Restaurante()
+
+restaurantes = [restaurante_brasil, restaurante_panama]
+
+print(restaurantes)
