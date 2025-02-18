804a3585a7041f786026ce303898275c5b96eeb3
nicolascacreis <reisnicolas112@gmail.com>
2025-02-17 22:01:28 -0300

    Update restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -1,14 +1,12 @@
 class Restaurante: 
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __inif__(self, nome, categoria):
+        self.nome = ''
+        self.categoria = ''
+        self.ativo = False
 
-restaurante_panama = Restaurante()
-restaurante_panam.nome = 'nome'
-restaurante_panama.categoria = 'Culturas'
-
-restaurante_brasil = Restaurante()
+restaurante_panama = Restaurante('panama', 'cultural')
+restaurante_brasil = Restaurante('brasil', 'cultural')
 
 restaurantes = [restaurante_brasil, restaurante_panama]
 
-print(restaurantes)
+print(restaurante_brasil)
